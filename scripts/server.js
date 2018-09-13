e358f5d1213cf2e68a84ceedb086339766b8906f
RoNiN-MD-7 <ronin.md.7@gmail.com>
2018-09-13 14:24:42 +0300

    Add fonts

--- scripts/server.js
+++ scripts/server.js
@@ -9,12 +9,25 @@ myMapImg.set('/img/otter3.jpg', './img/otter3.jpg');
 myMapImg.set('/img/otter4.jpg', './img/otter4.jpg');
 myMapImg.set('/img/otter5.jpg', './img/otter5.jpg');
 
+var myMapFonts = new Map();
+myMapFonts.set('/stylesheets/fonts/LAKESHOR-webfont.woff', './stylesheets/fonts/LAKESHOR-webfont.woff');
+myMapFonts.set('/stylesheets/fonts/Airstream-webfont.woff', './stylesheets/fonts/Airstream-webfont.woff');
+myMapFonts.set('/stylesheets/fonts/LAKESHOR-webfont.ttf', './stylesheets/fonts/LAKESHOR-webfont.ttf');
+myMapFonts.set('/stylesheets/fonts/Airstream-webfont.ttf', './stylesheets/fonts/Airstream-webfont.ttf');
+
 const server = http.createServer(function(request, response) {
   console.log(request.method, request.url);
   for (var [key, value] of myMapImg) {
     if (request.url == key) {
       const img = fs.readFileSync(value);
       response.end(img);
+    }
+  }
+
+	for (var [key, value] of myMapFonts) {
+    if (request.url == key) {
+      const font = fs.readFileSync(value);
+      response.end(font);
     }
   }
 
